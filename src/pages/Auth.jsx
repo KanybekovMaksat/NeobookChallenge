2435f2aacefa6409eca36931e51aee755ecf2263
kanybekovmaksat <knb.max004@gmail.com>
2023-12-12 20:17:00 +0600

    feat: add Auth component with MUI Tabs and Background Image
    Implemented the Auth component with MUI Tabs for login and registration.
    Added Background Image and necessary imports.

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import Box from '@mui/material/Box';
+import React, { useState } from 'react'
+import { Box, Tab, Tabs } from '@mui/material';
 
 import BackgroundImages from "../assets/images/background-auth.png";
 
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 const Auth = () => {
-  return (
-    <>
-        <img src={BackgroundImages} alt="" />
-        <div className="auth-content">
-            
-        </div>
-    </>
-  )
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{ overflowY: "hidden", display: "flex", columnGap: "300px" }}>
+            <img src={BackgroundImages} alt="" />
+            <Box >
+                <Box sx={{ borderBottom: 1, borderColor: 'divider', mt: 20 }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Войти" {...a11yProps(0)} />
+                        <Tab label="Регистрация" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+            </Box>
+        </Box>
+    )
 }
 
 export default Auth
